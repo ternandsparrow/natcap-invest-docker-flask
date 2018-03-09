6295edc4ca3591f8482786b82e21af2bc3d737c6
Tom Saleeba <tom.saleeba@gmail.com>
2018-03-09 11:07:42 +1030

    chore: improve performance of setup.sh script by going more parallel

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,14 +4,30 @@ cd `dirname "$0"`
 apt-get update
 # apt-get dist-upgrade # TODO might improve security
 
+data_dir=/data/pollination
+our_dir=/tmp/foo
+raster_archive=$our_dir/raster.zip
+
+function get_raster {
+  mkdir $our_dir
+  pushd $our_dir
+  wget \
+    -O $raster_archive \
+    'https://github.com/tomsaleeba/landuse-raster-south-australia/releases/download/20180307-33m/landuse_raster_south_australia_33m_20180307.zip'
+  unzip $raster_archive
+  tif_count=`bash -c 'ls -1 *.tif | wc -l'`
+  if [ "$tif_count" != "1" ]; then
+    echo "[ERROR] expected exactly 1 .tif file, found $tif_count."
+    exit 1
+  fi
+  gzip *.tif
+  mv *.tif.gz $data_dir/south_australia_landcover.tif.gz
+  popd
+}
+
 apt-get --assume-yes install \
   wget
-our_dir=/tmp/foo
-mkdir $our_dir
-raster_archive=$our_dir/raster.zip
-wget \
-  -O $raster_archive \
-  'https://github.com/tomsaleeba/landuse-raster-south-australia/releases/download/20180307-33m/landuse_raster_south_australia_33m_20180307.zip' &
+get_raster &
 
 apt-get --assume-yes install \
   gdal-bin \
@@ -19,23 +35,17 @@ apt-get --assume-yes install \
   python-dev \
   gcc \
   unzip
-pip install -r requirements.txt
-data_dir=/data/pollination
-pushd $data_dir
-rm -rf *
-popd
 
-pushd $our_dir
-wait # for wget to finish
-unzip $raster_archive
-tif_count=`bash -c 'ls -1 *.tif | wc -l'`
-if [ "$tif_count" != "1" ]; then
-  echo "[ERROR] expected exactly 1 .tif file, found $tif_count."
-  exit 1
-fi
-gzip *.tif
-mv *.tif.gz $data_dir/south_australia_landcover.tif.gz
-popd
+function install_deps {
+  pip install -r requirements.txt
+  pushd $data_dir
+  rm -rf *
+  popd
+}
+
+install_deps &
+wait
+
 mv *.csv $data_dir
 apt-get --assume-yes purge \
   python-setuptools \
